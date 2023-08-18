f36cfd407414580e9c91a780b3cd9780c89ea3e8
Felix Adnel <felixadnel@Felixs-MacBook-Pro.local>
2023-08-18 03:29:16 -0500

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+
+		}
+	
+	
+		public Car(string makeInput, string modelInput, int yearInput)
+		{
+			Make = makeInput;
+			Model = modelInput;
+			Year = yearInput;
+		}
+
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+
+
+		 
+
+	}
+
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Dodge";
+            myCar.Model = "Scatpack";
+            myCar.Year = 2021;
+
+            var nissan = new Car()
+            {
+                Make = "Nissan",
+                Model = "350z",
+                Year = 2006
+            };
+
+            var chevy = new Car("chevy", "zl1", 2022);
+
+            var carList = new List<Car>() { myCar, chevy, nissan };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
         }
     }
 }
